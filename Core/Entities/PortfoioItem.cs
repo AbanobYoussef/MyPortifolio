7bcdbd9ffdcb7f8f806f8293c9715d1f210715fe
AbanobYoussef <abanobyoussef1996@gmail.com>
2020-06-09 17:08:11 +0200

    Add project files.

---
+++ Core/Entities/PortfoioItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Entities
+{
+    public class PortfoioItem : EntityBase
+    {
+        public string ProjectName { get; set; }
+        public string Descrption { get; set; }
+        public string ImageUrl { get; set; }
+    }
+
+
+}
